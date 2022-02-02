f634c770c257714a5cc63859dce140e85ead5c66
Ranveer1801 <87312402+Ranveer1801@users.noreply.github.com>
2022-02-02 16:38:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var SpeechRecognition=window.WebKitSpeechRecognition;
+var recognition=new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+
+recognition.onresult=function run(event){
+    console.log(event);
+
+    var Content=event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML=Content;
+}
